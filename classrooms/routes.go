60d3a0a3d2fd1298dec50e24ad2c3ea0a720b4bb
EzequielBPullolil <programacionezequielk@gmail.com>
2024-01-24 19:12:45 -0300

    Rename function

--- classrooms/routes.go
+++ classrooms/routes.go
@@ -1,19 +1,16 @@
 package classroom
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
-func ImportUserRoutes(router *gin.Engine) {
+func ImportClassRoomRoute(router *gin.Engine) {
 	// Grupo de rutas para operaciones de usuario
 	r := router.Group("/classrooms")
 
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Operaciones de usuario",
-		})
+	/// FindAllClassrooms
+	r.GET("", func(c *gin.Context) {
+		c.String(200, "ok")
 	})
 
 }
